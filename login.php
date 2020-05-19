b61883abb20ab1e9c464a2fc32e7f03c6ced515f
junho <plusjob70@naver.com>
2020-05-19 20:42:18 +0900

    login v2

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+	try{
+		$db = new PDO("mysql:dbname=cgv; host=localhost; port=3306", "root", "a12345");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$ID = $_GET['ID'];
+		$PW = $_GET['PW'];
+
+		$rows = $db->query("SELECT * FROM logintable");
+        $result = $rows->fetchAll();
+
+		for($i = 0; $i<count($result); $i++){
+        	if($result[$i]["ID"] == $ID and $result[$i]["PW"] == $PW){
+        		header("Location: index.html");
+        		//echo "jfoefiejiofjeiofjioejf";
+        	}
+
+        }
+
+
+	} catch (PDOException $ex) {
+        ?>
+        <p>Sorry, a database error occurred. Please try again later.</p>
+        <p>(Error details: <?= $ex->getMessage() ?>)</p>
+        <?php
+    }
+    exit;
+?>
